79ae84da9b3992dca208e5bf0d62e3e6e53eec0b
NsSuraj-01 <73475383+NsSuraj-01@users.noreply.github.com>
2024-11-04 11:14:16 +0530

    added problem

--- DP/Problems/DpGrids.java
+++ DP/Problems/DpGrids.java
@@ -138,6 +138,46 @@ public class DpGrids {
         System.out.println("tab: " + tab);
     }
 
+    public static int pathSumRec(int grid[][], int i, int j) {
+        if(i == 0 && j == 0) return grid[i][j];
+        if(i < 0 || j < 0) return (int)1e9;
+
+        int up = pathSumRec(grid, i-1, j);
+        int left = pathSumRec(grid, i, j-1);
+        
+        return grid[i][j] + Math.min(up, left);
+    }
+
+    public static int pathSumTab(int grid[][]) {
+        int n = grid.length, m = grid[0].length;
+        int dp[][] = new int[n][m];
+
+        for(int rw[] : dp) {
+            Arrays.fill(rw, (int)1e9);
+        }
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<n; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j=1; j<m; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+
+    // min path sum
+    public static  int minPathSum(int grid[][]) {
+        int n = grid.length, m = grid[0].length;
+        return pathSumRec(grid, n-1, m-1);
+    }
+
     public static void main(String[] args) {
         // int ac[][] = { {1,2,5}, {3,1,1}, {3,3,3} };
         // ninjasTraining(ac); 
